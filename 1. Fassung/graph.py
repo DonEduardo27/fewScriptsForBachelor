bbe13678c0be0d2bb1bb42007ffac06ddc8cd3a3
DonEduardo27 <michael.schwarzkopf@uni-weimar.de>
2018-06-19 13:25:56 +0200

    Initial commit mit erster Fassung

---
+++ 1. Fassung/graph.py
@@ -0,0 +1,123 @@
+import random
+import math
+from time import *
+import gc
+
+def main():
+	#Test ob Adjazenzliste aus Adjazenzmatrix ordnungsgemaess erstellt wird 
+	mat = [[1,0,1,1,0,1],[1,0,1,0,0,0],[0,1,1,1,1,0],[1,0,0,0,0,1],[1,0,1,1,0,0],[0,0,1,0,1,1]]
+
+	g = graph(mat, "G")
+	g.printmat()
+	g.printlist()
+
+#Zeitmessung
+	#erster Parameter: wie oft wird gemessen, 
+	#zweiter Parameter: In wie grossen abstaenden wird gemessen
+	timeTest(80,100)
+
+def timeTest(runs, stepsize):
+	#im Array times werden die zeiten in sekunden gespeichert
+	#fuer jede Runde wird die Zeit von InitRandomly gemessen, welche je nach runde groesser
+	#werdende graphen als liste initialisiert
+	times = []
+	for i in range(1,runs+1):
+		a = i * stepsize
+		g2 = graph(0, "G" + str(a))
+		t1 = clock()
+		g2.initRandomly(a)
+		times.append(clock() - t1)
+
+		#wird naturlich nicht mitgemessen
+		g2.printlist()
+		del g2
+		gc.collect()
+		try: 
+			print(str(g2.name) + " existiert noch")
+			return 0
+		except:
+			print "Objekt geloescht"
+
+		#diese Scheife dient zum ausgeben der Messwerte
+	for i in range(0,runs):
+		print ("Durchlauf " + str(i+1) + " dauerte "+str(times[i]) + " Sekunden (" + str(stepsize*(i+1)) + " Vertices erstellt)")
+
+class vertex:
+	#Jeder Vertex hat einige Nachbarn (somit eine Edge pro Nachbar)
+	#einen Index bzw Name und, falls es nochmal wichtig wird, ein Gewicht
+	def __init__(self,i):
+
+		self.neighbours = []
+		self.index = 0
+		self.weight = 0
+
+		self.index = i
+
+	def setNeighbour(self,v):
+		self.neighbours.append(v)
+		self.weight += 1
+
+	def printNeighbours(self):
+		prntstr = ""
+		for i in self.neighbours:
+			prntstr += str(i.index) + "  "
+		return prntstr
+
+class graph:
+	#Graphen haben Einen Namen, Vertices, und ueber deren nachbarn, Edges.
+	#durch angabe einer adjazenzmatrix kann ein graph generiert werden, ohne diese
+	#bleibt er leer
+	def __init__(self, mat, name):
+
+		self.name = name
+		self.adjmat = []
+		self.vertices = []
+
+		if mat != 0:
+			if len(mat) == len(mat[0]):
+				self.adjmat = mat
+				self.initListFromMat()
+			else:
+				print("Adjazenzmatrizen mussen quadratisch sein.")
+
+	def initListFromMat(self):
+		#Funktion um aus adjMat adjList zu machen
+		for i in range(0,len(self.adjmat)):
+			newVertex = vertex(i)
+			self.vertices.append(newVertex)
+		
+		for row in range(0,len(self.adjmat)):
+			for column in range(0,len(self.adjmat)):
+				if self.adjmat[row][column] == 1:
+					self.vertices[row].setNeighbour(self.vertices[column])
+	def initRandomly(self,size):
+		#initialisiert einen beliebig grossen graphen mit IVI^(1,5) vielen Kanten zufaellig
+
+		#vertices erstellen
+		for i in range(0, size):
+			newVertex = vertex(i)
+			self.vertices.append(newVertex)
+
+		#jedem dieser Vertices einige Vertices als Nachbarn zuweisen
+		for v in self.vertices:
+			probabilityEachStep = int( math.sqrt(size))
+			while random.randint(0,probabilityEachStep) != 0:
+				neighbourIndex = random.randint(0,size-1)
+				#while self.vertices[neighbourIndex] in v.neighbours:
+				#	neighbourIndex = random.randint(0,size-1)
+				v.setNeighbour(self.vertices[neighbourIndex])
+		# Wenn pro Vertex jeder sqrt(IVI) -te Vertex ein Nachbar ist, gilt etwa IEI = IVI^(1,5)
+
+	def printmat(self):
+		print("Adjazenzmatrix von "+ self.name + ":")
+		for i in self.adjmat:
+			print i
+	def printlist(self):
+		print("Adjazenzliste von "+ self.name + ":")
+		for i in self.vertices:
+			print(str(i.index) + ": " + i.printNeighbours())
+		
+
+
+main()
+
